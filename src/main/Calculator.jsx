42d68b7f8cab15f1b87610f36d2181a8c238af10
cabral2 <saviocampolina9@gmail.com>
2019-07-11 01:01:53 -0300

    First commit

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Calculator.css'
+
+export default class Calculator extends React.Component {
+    render(){
+        return(
+            <div className='calculator'>
+                
+            </div>
+
+        )
+    }
+}
---
+++ src/main/Display.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Display extends React.Component {
+    state {
+        result: 0; 
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
